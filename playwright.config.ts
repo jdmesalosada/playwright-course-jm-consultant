0ddfa64d107660c069ea329aa1847b0ce1176752
Julian <jdmesalosada@gmail.com>
2024-03-09 12:25:32 -0500

    take screenshots

--- playwright.config.ts
+++ playwright.config.ts
@@ -41,7 +41,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     //trace: 'on-first-retry',
-    trace: 'on'
+    trace: 'on',
+    //screenshot: 'only-on-failure'
   },
 
   /* Configure projects for major browsers */
--- tests/saucedemo1.spec.ts
+++ tests/saucedemo1.spec.ts
@@ -1,12 +1,19 @@
 import { test, expect } from '@playwright/test';
 
-test('purchase an item', async ({ page }) => {
+test('purchase an item', async ({ page }, testInfo) => {
   await page.goto('https://www.saucedemo.com/');
 
   await page.getByRole('textbox', {name: 'Username'}).fill('standard_user')
+  //await page.screenshot({path: 'screenshots/login_username.png'})
+
   await page.getByRole('textbox', {name: 'Password'}).fill('secret_sauce')
   await page.getByRole('button', {name: 'Login'}).click()
 
+  await testInfo.attach('login', {
+    body: await page.screenshot(),
+    contentType: 'image/png'
+  })
+  //await page.screenshot({path: 'screenshots/login.png', fullPage: true})
 
   const itemsContainer = await page.locator('#inventory_container .inventory_item').all()
 
