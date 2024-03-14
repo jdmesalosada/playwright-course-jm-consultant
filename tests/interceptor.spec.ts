f91bb4b4994fed9285053b3d949300fa05792363
Julian <jdmesalosada@gmail.com>
2024-03-14 16:05:53 -0500

    updated playwright version

--- tests/interceptor.spec.ts
+++ tests/interceptor.spec.ts
@@ -23,3 +23,42 @@ test("purchase an item 2", async ({ page }) => {
 
    await page.screenshot({path:'login.png', fullPage:true}) 
 });
+
+test("interceptor test", async ({ page }) => {
+
+    await page.route(
+      "https://demoqa.com/BookStore/v1/Books",
+      (route) => {
+        route.fulfill({
+            status: 304,
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: `
+
+            {
+                "books": [
+                    {
+                        "isbn": "9781449325862",
+                        "title": "El libro que jUlian nunca escribio",
+                        "subTitle": "A Working Introduction",
+                        "author": "Richard E. Silverman",
+                        "publish_date": "2020-06-04T08:48:39.000Z",
+                        "publisher": "O'Reilly Media",
+                        "pages": 500,
+                        "description": "This pocket guide is the perfect on-the-job companion to Git, the distributed version control system. It provides a compact, readable introduction to Git for new users, as well as a reference to common commands and procedures for those of you with Git exp",
+                        "website": "http://chimera.labs.oreilly.com/books/1230000000561/index.html"
+                    }
+                ]
+            }
+            `
+        })
+      }
+    );
+  
+    
+     await page.goto('https://demoqa.com/books') 
+
+     await page.pause()
+     await page.screenshot({path:'books.png', fullPage:true}) 
+  });
